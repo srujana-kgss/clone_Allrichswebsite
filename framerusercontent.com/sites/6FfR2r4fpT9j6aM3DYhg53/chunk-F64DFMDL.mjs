594b5d362c2dabe1333e420a7097de86deecda5b
srujana-kgss <srujanakgss@gmail.com>
2024-01-06 19:44:57 +0530

    clone_website

---
+++ framerusercontent.com/sites/6FfR2r4fpT9j6aM3DYhg53/chunk-F64DFMDL.mjs
@@ -0,0 +1,2 @@
+import{r as e,s as o}from"./chunk-IY4S7CPQ.mjs";import{f as a}from"./chunk-ENK4YSX5.mjs";a();var d="default"in o?e:o,t={},r=d;t.createRoot=r.createRoot;t.hydrateRoot=r.hydrateRoot;var R=t.createRoot,c=t.hydrateRoot;export{R as a,c as b};
+//# sourceMappingURL=chunk-F64DFMDL.mjs.map
---
+++ framerusercontent.com/sites/6FfR2r4fpT9j6aM3DYhg53/chunk-T4NZILQE.mjs
@@ -0,0 +1,2 @@
+import{f as t}from"./chunk-ENK4YSX5.mjs";t();function e(a,r){return{description:"Made with Framer",favicon:"https://framerusercontent.com/images/lPUfbEPf8elvYZPcHVDd28ebszY.png",title:"My Framer Site"}}export{e as a};
+//# sourceMappingURL=chunk-T4NZILQE.mjs.map
